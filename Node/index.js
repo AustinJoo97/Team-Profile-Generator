29f2fd71f4b97351355a623c1c4818e1b373622f
AustinJoo97 <austinjoo97@gmail.com>
2021-04-18 20:02:46 -0400

    added functionality to ask questions based on employer, employee, or intern status; next is to implement when and how these questions are asked

--- Node/index.js
+++ Node/index.js
@@ -1,7 +1,12 @@
 const inquirer = require('inquirer');
 const employeeCards = document.getElementById('employeeCards');
 
+function init(){
+    managerCardCreation();
+};
+
 function managerCardCreation(){
+    let nextMember;
     inquirer.prompt([
         {
             type: 'input',
@@ -26,5 +31,66 @@ function managerCardCreation(){
     ])
     .then((response) => {
         console.log(response);
+        // Create manager card
+        // Run inquirer to ask what type of employee next is
+        // Based on that answer, determine what nextMember must be set to in order to determine which function must be run
     })
-}
+};
+
+function engineerCardCreation(){
+    let nextMember;
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter Engineer\'s Name: ',
+            name: 'engineerName'
+        },
+        {
+            type: 'input',
+            message: 'Please enter Engineer\'s ID:',
+            name: 'engineerID'
+        },
+        {
+            type: 'input',
+            message: 'Please enter Engineer\'s email: ',
+            name: 'engineerEmail'
+        },
+        {
+            type: 'input',
+            message: 'Please enter Engineer\'s GitHub handle: ',
+            name: 'engineerGitHub'
+        }
+    ])
+    .then((response) => {
+        console.log(response);
+    })
+};
+
+function internCardCreation(){
+    let nextMember;
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter Intern\'s name: ',
+            name: 'internName'
+        },
+        {
+            type: 'input',
+            message: 'Please enter Intern\'s ID:',
+            name: 'internID'
+        },
+        {
+            type: 'input',
+            message: 'Please enter Intern\'s email: ',
+            name: 'internEmail'
+        },
+        {
+            type: 'input',
+            message: 'Please enter Intern\'s school name: ',
+            name: 'internSchool'
+        }
+    ])
+    .then((response) => {
+        console.log(response);
+    })
+};
