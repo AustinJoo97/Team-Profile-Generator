eb6b91d37a758932f0d8031986251540fbf16331
AustinJoo97 <austinjoo97@gmail.com>
2021-04-21 15:58:15 -0400

    added tests for engineer test file; employee, manager, and engineer tests all passing

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,1 +1,29 @@
 const Engineer = require('../Node/engineer');
+
+describe('arguments passed via super in Engineer constructor', () => {
+    it('should return new Engineer id', () => {
+        const newEngineer = new Engineer('Bob', '000', 'BobEngineer@gmail.com', 'bobtestgithub')
+
+        expect(newEngineer.getName()).toEqual('Bob');
+
+        expect(newEngineer.getID()).toEqual('000');
+
+        expect(newEngineer.getEmail()).toEqual('BobEngineer@gmail.com');
+    });
+});
+
+describe('.getGitHub() method', () => {
+    it('should return the github handle of new instance of Engineer', () => {
+        const newEngineer = new Engineer('Bob', '000', 'BobEngineer@gmail.com', 'bobtestgithub')
+
+        expect(newEngineer.getGitHub()).toEqual('bobtestgithub')
+    })
+})
+
+describe('.getRole() method', () => {
+    it('should return "Engineer" as the role, overriding the inherited "Employee" role from extended Employee constructor', () => {
+        const newEngineer = new Engineer('Bob', '000', 'BobEngineer@gmail.com', 'bobtestgithub')
+
+        expect(newEngineer.getRole()).toEqual('Engineer')
+    })
+})
