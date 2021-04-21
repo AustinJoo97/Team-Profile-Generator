6c3d36ce11c061f16c58a214f5c63054569fcdc4
AustinJoo97 <austinjoo97@gmail.com>
2021-04-21 15:41:59 -0400

    installed dependencies, updated package.json; all tests passing for employee test file

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,1 +1,34 @@
+const { getMaxListeners } = require('process');
 const Employee = require('../Node/employee');
+
+describe('getName() method', () => {
+    it('should return a newly created employees name', () => {
+        const newEmployee = new Employee('Jim', '000', 'jimtest@gmail.com')
+
+        expect(newEmployee.getName()).toEqual('Jim')
+    })
+})
+
+describe('.getID() method', () => {
+    it('should return the id for a new instance of Employee', () => {
+        const newEmployee = new Employee('Jim', '000', 'jimtest@gmail.com')
+
+        expect(newEmployee.getID()).toEqual('000')
+    })
+})
+
+describe('.getEmail() method', () => {
+    it('should return the email address for a new instance of Employee', () => {
+        const newEmployee = new Employee('Jim', '000', 'jimtest@gmail.com')
+
+        expect(newEmployee.getEmail()).toEqual('jimtest@gmail.com')
+    })
+})
+
+describe('.getRole() method', () => {
+    it('should return "employee" as the role for the new instance of Employee', () => {
+        const newEmployee = new Employee('Jim', '000', 'jimtest@gmail.com')
+
+        expect(newEmployee.getRole()).toEqual('Employee')
+    })
+})
--- test/manager.test.js
+++ test/manager.test.js
@@ -1,1 +1,4 @@
+const { describe } = require('yargs');
 const Manager = require('../Node/manager');
+
+describe(''
